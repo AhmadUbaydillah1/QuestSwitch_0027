b67f8350cf4bb6db35b5cf2f466bd016f0f61098
AhmadUbaydillah1 <abidwedani@gmail.com>
2025-11-05 17:52:53 +0700

    menambahkan variabel baru dan operator if else

--- asisten.java
+++ asisten.java
@@ -10,16 +10,42 @@ public class asisten {
         System.out.println("masukkan nilai akhir Matkul");
         System.out.println("masukan nilai Struktur Data= ");
         Float nSD = ns.nextFloat();
+        Float rnSD = nSD * 4 /100;
+
         System.out.println("masukan nilai Pemrograman Berorientasi Objek= ");
         Float nPBO = ns.nextFloat();
+        Float rnPBO = nPBO * 4 /100;
+
         System.out.println("masukan nilai Basis Data= ");
-        Float nBd = ns.nextFloat();
+        Float nBD = ns.nextFloat();
+        Float rnBD = nBD* 4 /100;
+
         System.out.println("masukan nilai Algoritma dan Pemrograman");
         Float nAP = ns.nextFloat();
+        Float rnAP = nAP * 4 /100;
+
         System.out.println("masukan nilai Rekayasa Perangkat Lunak");
         Float nRPL = ns.nextFloat();
+        Float rnRPL = nRPL * 4 /100;
 
-        rataRata = (nSD + nPBO+ nBd + nAP + nRPL) / 5;
-        System.out.println("Rata-rata anda= " +rataRata);
+        rataRata = (rnSD + rnPBO+ rnBD + rnAP + nRPL) * 4 / 100;
+        System.out.print("Rata-rata anda= " +rataRata);
+
+    if (rataRata >= 3.75 && rataRata <=4.00) {
+        System.out.println("A (Sangat baik)");
+    } if (rataRata >3.50 && rataRata <3.74){
+        System.out.println("B (Baik Sekali)");
+    } if (rataRata >3.00 && rataRata <3.49){
+        System.out.println("B (Baik)");
+    } if (rataRata >2.50 && rataRata <2.99){
+        System.out.println("BC (Cukup)");
+    } if (rataRata <2.50){
+        System.out.println("C (Kurang)");
+    } else {
+        System.out.println("Invalid");
     }
+
+
+    }
+
 }
