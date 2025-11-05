e09b022f08f2a9a93d7e1080b7beb2669494222a
AhmadUbaydillah1 <abidwedani@gmail.com>
2025-11-05 17:18:23 +0700

    menambahkan rumus rata-rata untuk nilai yang di input

--- asisten.java
+++ asisten.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class asisten {
     public static void main(String[] args) {
-        
+        Scanner ns = new Scanner(System.in);
+
+        double nilaiAkhir, rataRata;
+        Float nilai;
+
+        System.out.println("masukkan nilai akhir Matkul");
+        System.out.println("masukan nilai Struktur Data= ");
+        Float nSD = ns.nextFloat();
+        System.out.println("masukan nilai Pemrograman Berorientasi Objek= ");
+        Float nPBO = ns.nextFloat();
+        System.out.println("masukan nilai Basis Data= ");
+        Float nBd = ns.nextFloat();
+        System.out.println("masukan nilai Algoritma dan Pemrograman");
+        Float nAP = ns.nextFloat();
+        System.out.println("masukan nilai Rekayasa Perangkat Lunak");
+        Float nRPL = ns.nextFloat();
+
+        rataRata = (nSD + nPBO+ nBd + nAP + nRPL) / 5;
+        System.out.println("Rata-rata anda= " +rataRata);
     }
 }
